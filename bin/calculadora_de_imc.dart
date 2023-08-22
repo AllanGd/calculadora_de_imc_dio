0ecf59ab1f0fca8c834be7720b6b6f6ddbeed40c
AllanGd <allan_gd@hotmail.com>
2023-08-22 19:48:27 -0300

    first commit

---
+++ bin/calculadora_de_imc.dart
@@ -0,0 +1,5 @@
+import 'package:calculadora_de_imc/calculadora_de_imc.dart' as calculadora_de_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculadora_de_imc.calculate()}!');
+}
